e6ebddcdc8ad8879e8667de7e044d76baf2b4c2d
AtamjonRaximov <raximovatam999@gmail.com>
2025-02-14 07:24:13 +0500

    add rectangle.py

---
+++ rectangle.py
@@ -0,0 +1,11 @@
+class Rectangle:
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
+    def getSquare(self):
+        return self.a *self.b
+        
+    def getPerimeter(self):
+        return self.a + self.b * 2
+        
+        
