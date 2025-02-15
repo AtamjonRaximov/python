d60e60bc276888a230f348eb22d4d5339fcfa255
AtamjonRaximov <raximovatam999@gmail.com>
2025-02-15 15:14:56 +0500

    add user.py

---
+++ user.py
@@ -0,0 +1,42 @@
+# class User():
+#     def __init__(self,name,age,course=1):
+#         self.name = name
+#         self.age = age
+#         self.__course =course
+    
+#     def getcourse(self):
+#         return self.__course
+#     def setcourse(self,s):
+#         return s
+        
+        
+
+
+
+
+# class User():
+#     name = None
+#     age = None
+#     phone = None
+#     email = None
+     
+#     def  correctName(self):
+#         if len(self.name) > 3:
+#             return True
+#         else:
+#             return False
+    
+# class number():
+#     a = None
+#     b = None
+    
+#     def qosh(self):
+        
+#         return self.a+self.b
+    
+
+# class User:
+#     def __init__(self,name,age,party="Happy New Year"):
+#         self.name = name
+#         self.age = age
+#         self.party =party
