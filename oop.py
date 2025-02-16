28fda2d078596444c48716b1f740c447172cf334
AtamjonRaximov <raximovatam999@gmail.com>
2025-02-16 13:05:56 +0500

    add opp.py

---
+++ oop.py
@@ -0,0 +1,94 @@
+# from user import User
+# from person import Person
+# from rectangle import Rectangle
+# from bank import Bank
+# from user import User
+from product import Product
+from card import Card
+
+
+p1=Product("olma",5000,2)   
+card =Card() 
+print(card.saqlash(p1))
+
+
+
+
+# p1=Product("olma",5000,2)
+# p2=Product("behi",15000,1)
+
+
+# U1=User("Atamjon",19)
+
+# print(U1.setcourse(1))
+
+
+
+# b1 =Bank("atamjon",15000)
+
+# print(b1.yechmoq(5000))
+# print(b1.yechmoq(11000))
+
+
+
+
+# R1 =Rectangle(10,20)
+
+# print(R1.getPerimeter())
+# print(R1.getSquare())
+
+
+
+
+# p1=Person("Atamjon",19)
+# print(p1.checkAge())
+# from employe import Employ
+
+
+# U1 =User("Atamjon",19)
+# print(U1.name)
+# print(U1.age)
+# print(U1.party)
+
+
+# p1 = Employ("atamjon",18,150000)
+# print(p1.salary)
+
+# p2 = Employ("alisher",18,5000)
+# print(p2.salary)
+
+# p3 = Employ("zokir",18,8000)
+# print(p3.salary)
+
+# p4 = Employ("ali",18,3000)
+# print(p4.salary)
+
+
+# print(p1.salary + p2.salary +p3.salary +p4.salary)
+
+
+
+# user1 = User()
+# son = number()
+
+# user1.name ="Atamjon"
+# user1.age =19
+# user1.email ="raximovatam99@gmail.com"
+# user1.phone =9399990315
+
+
+# print(user1.name)
+# print(user1.age)
+# print(user1.email)
+# print(user1.phone)
+
+
+# son.a =5
+# son.b = 10
+
+# print(son.qosh())
+
+
+# user1.name = "kid"
+
+# print(user1.correctName())
